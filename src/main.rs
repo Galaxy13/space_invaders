24357a10321da894ec04192baf10b4d9411af96b
Galaxy13 <gen.hort54@gmail.com>
2023-04-26 18:41:04 +0300

    Update: creating of frame, player, shot handlers and basic render

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::error::Error;
 use rusty_audio::Audio;
-use std::fs;
+use std::{fs, io, thread};
+use std::sync::mpsc;
+use std::time::{Duration, Instant};
+use crossterm::{event, ExecutableCommand, terminal};
+use crossterm::cursor::Hide;
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use space_invaders::{frame, render};
+use space_invaders::frame::{Drawable, new_frame};
+use space_invaders::player::Player;
 
 fn main() -> Result<(), Box<dyn Error>>{
     let mut audio = Audio::new();
@@ -10,9 +19,77 @@ fn main() -> Result<(), Box<dyn Error>>{
         println!("{}", &path.trim_end_matches(".wav")[7..]);
         audio.add(&path.trim_end_matches(".wav")[7..], &path)
     };
-    audio.play("explode");
+    audio.play("startup");
 
-    //Cleanup
+    // Output render
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    //Render Loop in another Thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => {
+                    x
+                },
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        };
+    });
+
+    //Game Loop
+    let mut player = Player::new();
+    let mut instant = Instant::now();
+    'gameloop: loop {
+        // First frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
+        let mut curr_frame = new_frame();
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    },
+                    KeyCode::Left => {
+                      player.move_left();
+                    },
+                    KeyCode::Right => {
+                        player.move_right();
+                    },
+                    KeyCode::Char(' ') => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        //Timer Update
+        player.update(delta);
+        // Draw a render
+        player.draw(&mut curr_frame);
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    //Cleanup of thread
+    drop(render_tx);
+    render_handle.join().unwrap();
+    //Cleanup of audio
     audio.wait();
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     return Ok(())
 }
